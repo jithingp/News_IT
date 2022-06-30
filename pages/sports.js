24253ddbb8e0c1c7e7bc31b07acf75ef4aa4c48c
jithingp <jithingpanoli18@gmail.com>
2022-06-30 20:16:33 +0530

    Added crypto,politics,sports pages

---
+++ pages/sports.js
@@ -0,0 +1,35 @@
+import { Flex,Box, Text } from "@chakra-ui/react"
+import axios from "axios"
+import Article from "../components/Article"
+
+const fetchApi = async (url)=>{
+  const {data} = await axios.get((url),{
+    headers: {
+      'X-RapidAPI-Key': process.env.SMITH,
+      'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+    }
+  })
+  return data;
+}
+
+export default function Sports({allNews}) {
+  return (
+   <Box>
+        <Text fontSize='2xl' p='4' fontWeight='bold' >
+            Sports
+        </Text>
+        <Flex flexWrap="wrap" marginTop='3'>
+            {allNews.articles.map((article)=> <Article article={article} key={article._id}/>)}
+        </Flex>
+   </Box>
+  )
+}
+
+export async function getServerSideProps(){
+  const allNews = await fetchApi('https://free-news.p.rapidapi.com/v1/search?q=sports&lang=en');
+  return {
+    props: {
+      allNews
+    }
+  }
+}
